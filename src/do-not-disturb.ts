55f2a2e28a5e214ab9b1927efd691d08d254c9b3
Vivek Ranjan <vivek@vivekranjan.me>
2021-08-31 22:14:57 -0400

    Fixed DND status not getting updated on big sur

--- src/do-not-disturb.ts
+++ src/do-not-disturb.ts
@@ -1,5 +1,8 @@
 const os = require('os')
 const dnd = require('mindful-timer/src/dnd')
+const semverParse = require('semver/functions/parse');
+const fs = require('fs')
+const exec = require('child_process').exec
 
 export const enable = function() {
   if (os.type() === 'Darwin') {
@@ -10,5 +13,12 @@ export const enable = function() {
 export const disable = function() {
   if (os.type() === 'Darwin') {
     dnd.getDndProvider().disable()
+    const { major } = semverParse(os.release())
+    if (major >= 20) {
+      if (!fs.existsSync('/tmp/makerflow-controlcenter-killed.txt')) {
+        exec('killall ControlCenter')
+        fs.writeFileSync('/tmp/makerflow-controlcenter-killed.txt', '')
+      }
+    }
   }
 }
